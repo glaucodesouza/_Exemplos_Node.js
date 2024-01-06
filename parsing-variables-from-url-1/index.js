eeeafe5700e20da0b3d1abb1ae301e2674c6e71a
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-01-06 18:32:01 -0300

    parsing variables from url

--- parsing-variables-from-url-1/index.js
+++ parsing-variables-from-url-1/index.js
@@ -42,6 +42,7 @@ const replaceTemplate = (temp, product) => {
 const tempOverview  = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard      = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const tempProduct   = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 //data is MORE  efficient here because the data is read in the start of application...
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 
@@ -54,12 +55,18 @@ const dataObj = JSON.parse(data);
 const server = http.createServer((req, res) => {
   // req: request
   // res: response
-  const pathName = req.url;
-
+  
+  // console.log(req.url);
+  // console.log(url.parse(req.url, true)); //true = pass query part from url like: ?id=0
+  // const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
+  
+  //---------------------------------------------------
   // Overview Page
+  //---------------------------------------------------
   // if user writes 127.0.0.1:8000
   // or if user writes 127.0.0.1:8000/overview on the browser
-  if (pathName === '/' || pathName === '/overview') {
+  if (pathname === '/' || pathname === '/overview') {
     res.writeHead(200, {'Content-type': 'text/html'});
     
     // here replaceTemplate(tempCard, el) is returned by the arrow function
@@ -70,19 +77,28 @@ const server = http.createServer((req, res) => {
     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
 
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-    console.log(cardsHtml);
+    // console.log(cardsHtml);
 
     res.end(output);
     // res.end(tempOverview);
 
+  //---------------------------------------------------
   // Product Page
+  //---------------------------------------------------
   // if user writes 127.0.0.1:8000/product on the browser
-  } else if (pathName === '/product') {
-    res.end('This is the PRODUCT');
-
+  } else if (pathname === '/product') {
+    res.writeHead(200, {'Content-type': 'text/html'});
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+    // console.log(query);
+    // res.end('This is the PRODUCT');
+  
+  //---------------------------------------------------
   // API
+  //---------------------------------------------------
   // if user writes 127.0.0.1:8000/api on the browser
-  } else if (pathName === '/api') {
+  } else if (pathname === '/api') {
     
     //without this, the characteres written back to page would be wrong...
     res.writeHead(200, {'Content-type': 'application/json'}); 
@@ -90,7 +106,9 @@ const server = http.createServer((req, res) => {
     //send back to browser, the data read...as a page.
     res.end(data); 
 
+  //---------------------------------------------------
   // Not Found
+  //---------------------------------------------------
   } else {
     //setting Header before the response
     res.writeHead(404, {
---
+++ parsing-variables-from-url-1/readme.md
@@ -0,0 +1,8 @@
+Class from UDEMY Course
+  Node.js, Express, MongoDB & More: The Complete Bootcamp 2024
+  Module 2, video 16
+  https://deloittedevelopment.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15080942
+
+Instrutor
+Jonas Schmedtmann
+Web Developer, Designer, and Teacher
