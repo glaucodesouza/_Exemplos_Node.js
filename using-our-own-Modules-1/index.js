095f3beb1bd9a98739ff5d3ebb78142b06750be8
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-01-06 18:58:28 -0300

    using-our-own-Modules-1

--- using-our-own-Modules-1/index.js
+++ using-our-own-Modules-1/index.js
@@ -1,10 +1,28 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const replaceTemplate = require('./modules/replaceTemplate')
 
 //////////////////////////////////
 //FILES
 //////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -17,26 +35,26 @@ const fs = require('fs');
 //////////////////////////////////
 
 
-const replaceTemplate = (temp, product) => {
+// const replaceTemplate = (temp, product) => {
 
-  // /g meaning of replacing all products in one pass...as a loop
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
+//   // /g meaning of replacing all products in one pass...as a loop
+//   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+//   output = output.replace(/{%IMAGE%}/g, product.image);
+//   output = output.replace(/{%PRICE%}/g, product.price);
+//   output = output.replace(/{%FROM%}/g, product.from);
+//   output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+//   output = output.replace(/{%QUANTITY%}/g, product.quantity);
+//   output = output.replace(/{%DESCRIPTION%}/g, product.description);
+//   output = output.replace(/{%ID%}/g, product.id);
 
-  // OBS.: 
-  // not-organic is a CSS class in template-product.html
-  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+//   // OBS.: 
+//   // not-organic is a CSS class in template-product.html
+//   if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
   
-  output = output.replace(/{%IMAGE%}/g, product.image);
+//   output = output.replace(/{%IMAGE%}/g, product.image);
 
-  return output; //return the replaced HTML
-}
+//   return output; //return the replaced HTML
+// }
 
 // Templates
 const tempOverview  = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
