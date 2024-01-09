69246e469afdc4f7686a64942d6d40e9d2121304
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-01-08 23:56:55 -0300

    prettier

--- third-party-modules-1/index.js
+++ third-party-modules-1/index.js
@@ -7,39 +7,15 @@ const fs = require('fs');
 const slugify = require('slugify'); //e.g.: id=0 calling as fresh-avocados
 
 // My own module
-const replaceTemplate = require('./modules/replaceTemplate')
+const replaceTemplate = require('./modules/replaceTemplate');
 
 //////////////////////////////////
 //FILES
-////////////////////////////////// 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//////////////////////////////////
 
 //////////////////////////////////
 //SERVER
 //////////////////////////////////
-
 
 // const replaceTemplate = (temp, product) => {
 
@@ -53,19 +29,19 @@ const replaceTemplate = require('./modules/replaceTemplate')
 //   output = output.replace(/{%DESCRIPTION%}/g, product.description);
 //   output = output.replace(/{%ID%}/g, product.id);
 
-//   // OBS.: 
+//   // OBS.:
 //   // not-organic is a CSS class in template-product.html
 //   if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  
+
 //   output = output.replace(/{%IMAGE%}/g, product.image);
 
 //   return output; //return the replaced HTML
 // }
 
 // Templates
-const tempOverview  = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCard      = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct   = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 //data is MORE  efficient here because the data is read in the start of application...
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
@@ -74,7 +50,7 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 //create a new Array of all products, but with slugify...
-const slugs = dataObj.map(el => slugify(el.productName, {lower: true})); 
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 console.log(slugs);
 
 // 1st create server
@@ -83,26 +59,26 @@ console.log(slugs);
 const server = http.createServer((req, res) => {
   // req: request
   // res: response
-  
+
   // console.log(req.url);
   // console.log(url.parse(req.url, true)); //true = pass query part from url like: ?id=0
   // const pathName = req.url;
   const { query, pathname } = url.parse(req.url, true);
-  
+
   //---------------------------------------------------
   // Overview Page
   //---------------------------------------------------
   // if user writes 127.0.0.1:8000
   // or if user writes 127.0.0.1:8000/overview on the browser
   if (pathname === '/' || pathname === '/overview') {
-    res.writeHead(200, {'Content-type': 'text/html'});
-    
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
     // here replaceTemplate(tempCard, el) is returned by the arrow function
     // thus, no need for curly braces
     //--THIS function replaces an entire LOOP for replace a template of cards for products
     //--This function returns an ARRAY with products with HTML formatting.
     //--THIS function returns an HTML for the user...
-    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
 
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
     // console.log(cardsHtml);
@@ -110,38 +86,37 @@ const server = http.createServer((req, res) => {
     res.end(output);
     // res.end(tempOverview);
 
-  //---------------------------------------------------
-  // Product Page
-  //---------------------------------------------------
-  // if user writes 127.0.0.1:8000/product on the browser
+    //---------------------------------------------------
+    // Product Page
+    //---------------------------------------------------
+    // if user writes 127.0.0.1:8000/product on the browser
   } else if (pathname === '/product') {
-    res.writeHead(200, {'Content-type': 'text/html'});
+    res.writeHead(200, { 'Content-type': 'text/html' });
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
     res.end(output);
     // console.log(query);
     // res.end('This is the PRODUCT');
-  
-  //---------------------------------------------------
-  // API
-  //---------------------------------------------------
-  // if user writes 127.0.0.1:8000/api on the browser
+
+    //---------------------------------------------------
+    // API
+    //---------------------------------------------------
+    // if user writes 127.0.0.1:8000/api on the browser
   } else if (pathname === '/api') {
-    
     //without this, the characteres written back to page would be wrong...
-    res.writeHead(200, {'Content-type': 'application/json'}); 
+    res.writeHead(200, { 'Content-type': 'application/json' });
 
     //send back to browser, the data read...as a page.
-    res.end(data); 
+    res.end(data);
 
-  //---------------------------------------------------
-  // Not Found
-  //---------------------------------------------------
+    //---------------------------------------------------
+    // Not Found
+    //---------------------------------------------------
   } else {
     //setting Header before the response
     res.writeHead(404, {
-      'Content-type':'text/html',
-      'my-own-header':'hello-world'
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
     });
     //response
     res.end('<h1>Page not found!</h1>');
