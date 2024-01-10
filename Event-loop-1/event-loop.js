9b92f3ccfd77cb8e791fe2b2813886466f36b15e
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-01-10 00:39:43 -0300

    Event-loop-1

--- Event-loop-1/event-loop.js
+++ Event-loop-1/event-loop.js
@@ -1,15 +1,37 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
-setTimeout(() => console.log('Timer 1 finished'), 0); //zero seconds
-setImmediate(() => console.log('Immediate 1 finished'));
+const start = Date.now(); //get date in miliseconds
+
+//set environment THREAD quantity variable to just 1
+//No more 4 threads for paralel processing
+process.env.UV_THREADPOOL_SIZE = 1;
+
+setTimeout(() => console.log(Date.now() - start, 'Timer 1 finished'), 0); //zero seconds
+setImmediate(() => console.log(Date.now() - start, 'Immediate 1 finished'));
 
 fs.readFile('test-file.txt', () => {
-  console.log('I/O finished');
   console.log('----------------------------------------------------------------');
+  console.log('I/O finished');
 
-  setTimeout(() => console.log('Timer 2 finished'), 0); //zero seconds
-  setTimeout(() => console.log('Timer 3 finished'), 3000); //3000 miliseconds = 3 seconds
-  setImmediate(() => console.log('Immediate 2 finished'));
+  setTimeout(() => console.log(Date.now() - start, 'Timer 2 finished'), 0); //zero seconds
+  setTimeout(() => console.log(Date.now() - start, 'Timer 3 finished'), 3000); //3000 miliseconds = 3 seconds
+  setImmediate(() => console.log(Date.now() - start, 'Immediate 2 finished'));
+
+  process.nextTick(() => console.log(Date.now() - start, 'Process.nextTick'));
+
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, `miliseconds... Password encrypted`);
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, `miliseconds... Password encrypted`);
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, `miliseconds... Password encrypted`);
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, `miliseconds... Password encrypted`);
+  });
 });
 
 console.log('Hello from the top-level code');
