3c6cf54e4f2c87e1963938900dcbafd0b0ec4e3b
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-01-11 21:55:53 -0300

    events

--- Events-1/events.js
+++ Events-1/events.js
@@ -10,5 +10,9 @@ myEmitter.on('newSale', () => {
   console.log('Costumer name: Jonas');
 });
 
-myEmitter.emit("newSale");
+myEmitter.on('newSale', stock => {
+  console.log(`There are now ${stock} items left in stock.`);
+});
+
+myEmitter.emit("newSale", 9);
 
