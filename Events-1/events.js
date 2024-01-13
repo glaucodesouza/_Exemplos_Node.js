2adcb939a9c056e2310478f668b1e4acdc233a69
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-01-12 23:27:18 -0300

    events

--- Events-1/events.js
+++ Events-1/events.js
@@ -1,4 +1,6 @@
 const EventEmitter = require('events');
+const http = require('http');
+const server = http.createServer();
 
 const myEmitter = new EventEmitter();
 
@@ -10,9 +12,26 @@ myEmitter.on('newSale', () => {
   console.log('Costumer name: Jonas');
 });
 
-myEmitter.on('newSale', stock => {
+myEmitter.on('newSale', (stock) => {
   console.log(`There are now ${stock} items left in stock.`);
 });
 
-myEmitter.emit("newSale", 9);
+myEmitter.emit('newSale', 9);
+////////////////////////////////////////////////////////////////////////
+server.on('request', (req, res) => {
+  console.log('Request received!');
+  console.log(req.url);
+  res.end('Request received!');
+});
 
+server.on('request', (req, res) => {
+  console.log('Another Request 😊');
+});
+
+server.on('close', () => {
+  console.log('Server closed!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Waiting for requests...');
+});
