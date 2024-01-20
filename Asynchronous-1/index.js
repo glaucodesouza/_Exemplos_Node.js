e7e6217b25e91af6bac95f9a5ede527609adfcc1
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-01-20 16:16:55 -0300

    Asynchronous callbacks in sequence

--- Asynchronous-1/index.js
+++ Asynchronous-1/index.js
@@ -1,6 +1,24 @@
 //Callback problem
-const fs = require("fs");
+const fs = require('fs');
+const superagent = require('superagent');
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  //here is an callback ...
   console.log(`Breed, ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      //here is another callback ...
+
+      if (err) {
+        return console.log(err.message);
+      }
+      console.log(res.body);
+
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        //here is another callback...
+        console.log('Random dog image saved to file!');
+      });
+    });
 });
