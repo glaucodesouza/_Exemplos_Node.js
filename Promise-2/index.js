3884f980b34455a7d077cf2178271558836d5354
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2024-02-03 15:22:14 -0300

    promise callbacks

--- Promise-2/index.js
+++ Promise-2/index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const superagent = require('superagent');
 
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  //here is a callback ...
+  //here is a callback ....
   console.log(`Breed, ${data}`);
 
   superagent
